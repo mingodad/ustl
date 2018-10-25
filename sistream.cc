abf9839e0e92467b47ee4f7e1778f5d61237ae8d
mingodad <mingodad@gmail.com>
2018-10-25 10:43:19 +0200

    Fix for the istringstream read/gcount bug.

--- sistream.cc
+++ sistream.cc
@@ -21,6 +21,15 @@ istringstream::istringstream (void) noexcept
     set_delimiters (DEFAULT_DELIMITERS);
 }
 
+istringstream::istringstream (const char* p) noexcept
+: istream()
+,_flags (0)
+{
+    exceptions (goodbit);
+    relink (p, strlen(p));
+    set_delimiters (DEFAULT_DELIMITERS);
+}
+
 istringstream::istringstream (const void* p, size_type n) noexcept
 : istream()
 ,_flags (0)
@@ -158,7 +167,7 @@ void istringstream::iread (string& v)
 istringstream& istringstream::read (void* buffer, size_type sz)
 {
     if (remaining() < sz && underflow(sz) < sz)
-	verify_remaining ("read", "", sz);
+	get (reinterpret_cast<char*>(buffer), sz, 0);
     else
 	istream::read (buffer, sz);
     return *this;
